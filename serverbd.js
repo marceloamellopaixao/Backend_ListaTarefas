4effc6998758225b2faed5d12ae62539984259cd
marceloamellopaixao <marceloamellopaixao@gmail.com>
2024-05-10 23:03:21 -0300

    att

--- serverbd.js
+++ serverbd.js
@@ -1,34 +1,36 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose()
+const cors = require('cors')
 
 const app = express();
 const PORT = 3000; // Defina a porta do servidor
+app.use(cors())
 
 const db = new sqlite3.Database('bd.db')
 
 // Criar a tabela 'tarefas' no banco de dados
 db.serialize(() => {
-    db.run("CREATE TABLE IF NOT EXISTS tarefas (id INTEGER PRIMARY KEY, tarefa TEXT)");
+    db.run("CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY, task TEXT)");
 });
 
 app.use(express.json());
 
 // Rota para adicionar uma nova tarefa
 app.post('/tarefas', (req, res) => {
-    const { tarefa } = req.body;
+    const { task } = req.body;
     // Inserir a nova tarefa no banco de dados
-    db.run("INSERT INTO tarefas (tarefa) VALUES (?)", [tarefa], function(err) {
+    db.run("INSERT INTO tasks (task) VALUES (?)", [task], function(err) {
         if (err) {
             return res.status(500).json({ error: err.message });
         }
-        res.status(201).json({ id: this.lastID, tarefa });
+        res.status(201).json({ id: this.lastID, task });
     });
 });
 
 // Rota para obter todas as tarefas
 app.get('/tarefas', (req, res) => {
     // Obter todas as tarefas do banco de dados
-    db.all("SELECT * FROM tarefas", [], (err, rows) => {
+    db.all("SELECT * FROM tasks", [], (err, rows) => {
         if (err) {
             return res.status(500).json({ error: err.message });
         }
@@ -40,7 +42,7 @@ app.get('/tarefas', (req, res) => {
 app.get('/tarefas/:id', (req, res) => {
     const { id } = req.params;
     // Obter a tarefa pelo ID
-    db.get("SELECT * FROM tarefas WHERE id = ?", [id], (err, row) => {
+    db.get("SELECT * FROM tasks WHERE id = ?", [id], (err, row) => {
         if (err) {
             return res.status(500).json({ error: err.message });
         }
@@ -55,9 +57,9 @@ app.get('/tarefas/:id', (req, res) => {
 // Rota para editar uma tarefa existente
 app.put('/tarefas/:id', (req, res) => {
     const { id } = req.params;
-    const { tarefa } = req.body;
+    const { task } = req.body;
     // Atualizar a tarefa com base no ID
-    db.run("UPDATE tarefas SET tarefa = ? WHERE id = ?", [tarefa, id], function(err) {
+    db.run("UPDATE tasks SET task = ? WHERE id = ?", [task, id], function(err) {
         if (err) {
             return res.status(500).json({ error: err.message });
         }
@@ -73,7 +75,7 @@ app.put('/tarefas/:id', (req, res) => {
 app.delete('/tarefas/:id', (req, res) => {
     const { id } = req.params;
     // Excluir a tarefa com base no ID
-    db.run("DELETE FROM tarefas WHERE id = ?", [id], function(err) {
+    db.run("DELETE FROM tasks WHERE id = ?", [id], function(err) {
         if (err) {
             return res.status(500).json({ error: err.message });
         }
